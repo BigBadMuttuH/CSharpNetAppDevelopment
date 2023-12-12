2e615b562186dcefb86dfb15bea36f2503a53d63
BigBadMuttuh <bigbadantonio@gmail.com>
2023-12-12 20:26:17 +0300

    2023-12-12_1

---
+++ 003_TaskParallelLibrary/03_HomeWork/Client/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Principal;
+
+namespace Client;
+
+internal static class Program
+{
+    private static void Main(string[] args)
+    {
+        var serverIpAddress = "127.0.0.1";
+        var serverProt = 9999;
+        // Console.WriteLine("Ваше имя: ");
+        // string userName = Console.ReadLine();
+        var userName = WindowsIdentity.GetCurrent().Name;
+
+        var client = new UdpChatClient(serverIpAddress, serverProt);
+        client.StartClient(userName);
+    }
+}
---
+++ 003_TaskParallelLibrary/03_HomeWork/Server/UdpChatServer.cs
@@ -0,0 +1,76 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server;
+
+internal class UdpChatServer
+{
+    private readonly CancellationTokenSource cancellationTokenSource;
+    private readonly UdpClient udpClient;
+
+    public UdpChatServer(int port)
+    {
+        udpClient = new UdpClient(port);
+        cancellationTokenSource = new CancellationTokenSource();
+    }
+
+    public async Task StartUdpServerAsync()
+    {
+        var ipEndPoint = new IPEndPoint(IPAddress.Any, 0);
+        Console.WriteLine("Ждем сообщений. Нажмите 'q' для выхода.");
+
+        var listenTask = ListenAsync(ipEndPoint, cancellationTokenSource.Token);
+
+        await WaitForExitAsync();
+        cancellationTokenSource.Cancel();
+
+        try
+        {
+            await listenTask;
+        }
+        catch (OperationCanceledException)
+        {
+            Console.WriteLine("Операция была отменена.");
+        }
+
+        udpClient.Close();
+        Console.WriteLine("Сервер остановлен.");
+    }
+
+    private async Task ListenAsync(IPEndPoint ipEndPoint, CancellationToken token)
+    {
+        while (!token.IsCancellationRequested)
+            if (udpClient.Available > 0)
+            {
+                var result = await udpClient.ReceiveAsync(token);
+                HandleClient(result.Buffer, result.RemoteEndPoint);
+            }
+            else
+            {
+                await Task.Delay(100, token); // Небольшая задержка для снижения нагрузки на процессор
+            }
+    }
+
+    private void HandleClient(byte[] buffer, IPEndPoint clientEndPoint)
+    {
+        var message = Encoding.UTF8.GetString(buffer);
+        var chatMessage = ChatMessage.FromJson(message);
+        Console.WriteLine(chatMessage?.Print() ?? "Не верный формат сообщения.");
+
+        var confirmation =
+            $"Сообщение '{chatMessage?.MessageText}' от {chatMessage?.SenderNickname} получено в {chatMessage?.Timestamp}.";
+        var confirmationBytes = Encoding.UTF8.GetBytes(confirmation);
+        udpClient.Send(confirmationBytes, confirmationBytes.Length, clientEndPoint);
+    }
+
+    private static async Task WaitForExitAsync()
+    {
+        await Task.Run(() =>
+        {
+            while (Console.ReadKey(true).Key != ConsoleKey.Q)
+            {
+            }
+        });
+    }
+}
